5e8bbc0a3e5629346aeac9384541c145e462a1d3
Mariana Gonçalves <68264920+MarianaGon95@users.noreply.github.com>
2021-08-18 18:40:51 -0300

    Variáveis
    Repare na variável apresentacao mostrada a seguir. Ela é uma string que representa um texto que pode mudar conforme o valor de outras variáveis.
    
    Identifique e crie todas as variáveis necessárias para que o texto de apresentacao seja exibido sem erros. Depois, crie a variável apresentacao exatamente como mostrado a seguir e a imprima da seguinte forma: console.log(apresentacao).
    
    apresentacao = "Meu nome é " + nomeCompleto + " (sou conhecido[a] como " + apelido + ") e tenho " + idade + " anos. Nasci no dia " + dataNascimento + ", na cidade de " + localNascimento + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregado" : "desempregado") + "."

---
+++ index.js
@@ -0,0 +1,11 @@
+let nomeCompleto="Mariana Gonçalves";
+let apelido = "Mari";
+let idade = 26;
+let dataNascimento = "18/03/1995";
+let localNascimento = "São Paulo";
+let altura = 1.62
+let trabalhando = true;
+
+apresentacao = "Meu nome é " + nomeCompleto + " (sou conhecido[a] como " + apelido + ") e tenho " + idade + " anos. Nasci no dia " + dataNascimento + ", na cidade de " + localNascimento + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregado" : "desempregado") + ".";
+
+console.log(apresentacao);
